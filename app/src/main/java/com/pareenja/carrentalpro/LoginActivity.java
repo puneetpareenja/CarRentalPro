b9a0878d519b0beccff588dd7ba2cdc97628a406
Puneet Pareenja <pareenjapuneet@gmail.com>
2019-07-06 11:05:50 -0700

    Added Code for User Authentication and Login

--- app/src/main/java/com/pareenja/carrentalpro/LoginActivity.java
+++ app/src/main/java/com/pareenja/carrentalpro/LoginActivity.java
@@ -3,19 +3,32 @@ package com.pareenja.carrentalpro;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.util.Patterns;
 import android.view.View;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.material.button.MaterialButton;
 import com.google.android.material.textfield.TextInputLayout;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseAuthInvalidCredentialsException;
+import com.google.firebase.auth.FirebaseAuthInvalidUserException;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
 
 import java.util.Objects;
 
 public class LoginActivity
         extends AppCompatActivity
         implements View.OnClickListener {
+
+    private static final String TAG = "LoginActivity";
 
     TextInputLayout emailTextInputLayout;
     TextInputLayout passwordTextInputLayout;
@@ -45,7 +58,12 @@ public class LoginActivity
         switch (v.getId()) {
             case R.id.material_button_login:
                 if (isEmailValid() & isPasswordValid()) {
+                    String email = Objects.requireNonNull(emailTextInputLayout.getEditText())
+                            .getText().toString().toLowerCase().trim();
+                    String password = Objects.requireNonNull(passwordTextInputLayout.getEditText())
+                            .getText().toString().trim();
 
+                    signIn(email, password);
                 }
                 break;
             case R.id.material_button_to_register:
@@ -53,6 +71,55 @@ public class LoginActivity
                 startActivity(intent);
                 break;
         }
+    }
+
+    private void signIn(final String email, String password) {
+        FirebaseAuth firebaseAuth = FirebaseAuth.getInstance();
+        firebaseAuth.signInWithEmailAndPassword(email, password)
+                .addOnSuccessListener(new OnSuccessListener<AuthResult>() {
+                    @Override
+                    public void onSuccess(AuthResult authResult) {
+                        signInForUser(Objects.requireNonNull(FirebaseAuth.getInstance()
+                                .getCurrentUser()).getUid());
+
+                    }
+                })
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        if (e instanceof FirebaseAuthInvalidUserException) {
+                            emailTextInputLayout.setError("User doesn't exists");
+                        } else if (e instanceof FirebaseAuthInvalidCredentialsException) {
+                            emailTextInputLayout.setError("Invalid Username or Password");
+                        }
+                        Log.d(TAG, "onFailure: " + e);
+                    }
+                });
+    }
+
+    private void signInForUser(String uid) {
+        FirebaseFirestore db = FirebaseFirestore.getInstance();
+        final DocumentReference documentReference = db.collection("users").document(uid);
+
+        documentReference.get().addOnSuccessListener(new OnSuccessListener<DocumentSnapshot>() {
+            @Override
+            public void onSuccess(DocumentSnapshot documentSnapshot) {
+                Person person = documentSnapshot.toObject(Person.class);
+
+                Intent intent;
+
+                if (person != null) {
+                    if (person.getPersonRole() == PersonRole.ADMINISTRATOR) {
+
+                    } else if (person.getPersonRole() == PersonRole.SALESPERSON) {
+
+                    } else {
+
+                    }
+                }
+            }
+        });
+
     }
 
     private boolean isPasswordValid() {
