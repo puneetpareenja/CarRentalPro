5a5db36b846b58f6fd5f833a0e577d77f16ea7ab
Puneet Pareenja <pareenjapuneet@gmail.com>
2019-07-06 00:51:25 -0700

    Created new Activity LoginActivity.java Created design for LoginActivity

--- app/src/main/java/com/pareenja/carrentalpro/LoginActivity.java
+++ app/src/main/java/com/pareenja/carrentalpro/LoginActivity.java
@@ -2,19 +2,13 @@ package com.pareenja.carrentalpro;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.Intent;
 import android.os.Bundle;
 
-
-public class SplashActivity extends AppCompatActivity {
+public class LoginActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_splash);
-
-        Intent intent = new Intent(this, LoginActivity.class);
-        startActivity(intent);
-
+        setContentView(R.layout.activity_login);
     }
 }
