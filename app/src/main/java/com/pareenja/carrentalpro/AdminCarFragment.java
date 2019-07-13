7d1f9ff4d0980f9c107c3bc87273f14b486077fc
Puneet Pareenja <pareenjapuneet@gmail.com>
2019-07-13 03:13:38 -0700

    Added a Extended FAB in fragment_admin_car.xml Added new drawable icon for the FAB. Created a new car_item_view.xml Created a new ViewAllCarAdapter Created a new RecyclerViewAdapater ViewAllCarAdapter.

--- app/src/main/java/com/pareenja/carrentalpro/AdminCarFragment.java
+++ app/src/main/java/com/pareenja/carrentalpro/AdminCarFragment.java
@@ -2,12 +2,24 @@ package com.pareenja.carrentalpro;
 
 
 import android.os.Bundle;
-
-import androidx.fragment.app.Fragment;
-
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
+
+import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.firebase.ui.firestore.FirestoreRecyclerOptions;
+import com.google.android.material.floatingactionbutton.ExtendedFloatingActionButton;
+import com.google.firebase.firestore.CollectionReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.Query;
+
+import java.util.ArrayList;
 
 
 /**
@@ -15,6 +27,14 @@ import android.view.ViewGroup;
  */
 public class AdminCarFragment extends Fragment {
 
+    private static final String TAG = "AdminCarFragment";
+    ArrayList<Car> carArrayList = new ArrayList<>();
+    RecyclerView recyclerView;
+    private FirebaseFirestore db = FirebaseFirestore.getInstance();
+    private CollectionReference carRef = db.collection("cars");
+    private ViewAllCarAdapter viewAllCarAdapter;
+    private View layout;
+    private ExtendedFloatingActionButton fab;
 
     public AdminCarFragment() {
         // Required empty public constructor
@@ -25,7 +45,60 @@ public class AdminCarFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_admin_car, container, false);
+        layout = inflater.inflate(R.layout.fragment_admin_car, container, false);
+        fab = layout.findViewById(R.id.fab_add_new_car);
+        fab.setOnClickListener(fabListener);
+        setUpRecyclerView();
+        return layout;
+    }
+
+    ExtendedFloatingActionButton.OnClickListener fabListener =
+            new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    Toast.makeText(getContext(), "Fab is working", Toast.LENGTH_SHORT).show();
+                }
+            };
+
+    private void setUpRecyclerView() {
+        Query query = carRef;
+
+        FirestoreRecyclerOptions<Car> carFirestoreRecyclerOptions =
+                new FirestoreRecyclerOptions.Builder<Car>()
+                        .setQuery(query, Car.class)
+                        .build();
+
+        Log.d(TAG, "setUpRecyclerView: Everything is working fine");
+        viewAllCarAdapter = new ViewAllCarAdapter(carFirestoreRecyclerOptions);
+        RecyclerView recyclerView = layout.findViewById(R.id.admin_car_recycler_view);
+        recyclerView.setHasFixedSize(true);
+
+        recyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
+        recyclerView.setAdapter(viewAllCarAdapter);
+
+        viewAllCarAdapter.setOnItemClickListener(new ViewAllCarAdapter.OnItemClickListener() {
+            @Override
+            public void onItemClick(DocumentSnapshot documentSnapshot, int position) {
+                Car car = documentSnapshot.toObject(Car.class);
+                car.setId(documentSnapshot.getId());
+
+//                Intent intent = new Intent(ViewCarsActivity.this, CarDetailsActivity.class);
+//                intent.putExtra("car", car);
+//                startActivity(intent);
+            }
+        });
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        viewAllCarAdapter.startListening();
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        viewAllCarAdapter.stopListening();
     }
 
 }
---
+++ app/src/main/java/com/pareenja/carrentalpro/ViewAllCarAdapter.java
@@ -0,0 +1,88 @@
+package com.pareenja.carrentalpro;
+
+import android.annotation.SuppressLint;
+import android.icu.text.NumberFormat;
+import android.os.Build;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.RequiresApi;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.firebase.ui.firestore.FirestoreRecyclerAdapter;
+import com.firebase.ui.firestore.FirestoreRecyclerOptions;
+import com.google.firebase.firestore.DocumentSnapshot;
+
+public class ViewAllCarAdapter extends FirestoreRecyclerAdapter<Car, ViewAllCarAdapter.ViewHolder> {
+
+    private OnItemClickListener listener;
+
+    public ViewAllCarAdapter(@NonNull FirestoreRecyclerOptions<Car> options) {
+        super(options);
+    }
+
+    @SuppressLint("SetTextI18n")
+    @RequiresApi(api = Build.VERSION_CODES.N)
+    @Override
+    protected void onBindViewHolder(@NonNull final ViewHolder viewHolder, int i, @NonNull Car car) {
+        viewHolder.carNameTextView.setText(car.getBrand() + " " + car.getCarModel());
+        viewHolder.carColorTextView.setText(car.getColor());
+        NumberFormat format = NumberFormat.getCurrencyInstance();
+
+        viewHolder.carPriceTextView.setText(
+                format.format(car.getPricePerHour())
+                        + " / "
+                        + format.format(car.getPricePerDay()));
+
+    }
+
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater
+                .from(parent.getContext())
+                .inflate(R.layout.car_item_view, parent, false);
+        return new ViewHolder(view);
+    }
+
+    public void setOnItemClickListener(OnItemClickListener listener) {
+        this.listener = listener;
+    }
+
+    public interface OnItemClickListener {
+        void onItemClick(DocumentSnapshot documentSnapshot, int position);
+    }
+
+    class ViewHolder extends RecyclerView.ViewHolder {
+
+        ImageView carImageView;
+        TextView carNameTextView;
+        TextView carColorTextView;
+        TextView carPriceTextView;
+
+        ViewHolder(@NonNull View itemView) {
+            super(itemView);
+
+            carImageView = itemView.findViewById(R.id.image_view_car_image);
+            carNameTextView = itemView.findViewById(R.id.text_view_car_name);
+            carColorTextView = itemView.findViewById(R.id.text_view_car_color);
+            carPriceTextView = itemView.findViewById(R.id.text_view_car_price);
+
+            itemView.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    int position = getAdapterPosition();
+                    if (position != RecyclerView.NO_POSITION
+                            && listener != null) {
+                        listener.onItemClick(getSnapshots().getSnapshot(position), position);
+                    }
+                }
+            });
+        }
+
+    }
+}
